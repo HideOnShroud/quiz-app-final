b240abe2b6ce81f6f97b35fe30bc1f1188badeec
Guga <guga.natroshvili.1@btu.edu.ge>
2023-01-13 17:45:51 +0400

    hopefully gameorebaze gaval

--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -6,6 +6,33 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div>
+        <img src="{{ $quiz->img }}" alt="f">
+        <div>
+            <h1>
+                {{ $quiz->title }}
+            </h1>
+        </div>
+        
+        <div>
+            <p>
+                {{ $quiz->desc }}
+            </p>
+        </div>
+        <div>
+            @foreach ($questions as $question)
+                <form action="">
+                    <p>{{ $question->question }}</p>
+                    <input type="radio" id="html" name="fav_language" value="HTML">
+                    <label for="html">{{ $question->answers }}</label><br>
+                    <input type="radio" id="css" name="fav_language" value="CSS">
+                    <label for="css">CSS</label><br>
+                    <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+                    <label for="javascript">JavaScript</label>
+                </form>
+                <p>{{ $question }}</p>
+            @endforeach
+        </div>
+    </div>    
 </body>
 </html>
